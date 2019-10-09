2869efac3b686146336fcda72673fb647741623f
oushu1zhangxiang1 <zhang.xiangxuan@oushu.io>
2019-10-09 15:29:48 +0800

    stock analysis

---
+++ time_sequence/stock.py
@@ -0,0 +1,8 @@
+# https://www.leiphone.com/news/201908/fdbuJVJUrBYPOiOK.html
+
+from sklearn.metrics import r2_score, median_absolute_error, mean_absolute_error, mean_squared_log_error
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
